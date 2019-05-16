28238b2c655ad1e251e3e93d5f648ab211d41375
Vedang Wartikar <vedangwartikar17@gmail.com>
2019-05-17 00:23:52 +0530

    updated]

--- gitFun.py
+++ gitFun.py
@@ -7,5 +7,5 @@ for i in range(1, 6):
         file.write('i')
         subprocess.call("git init", shell = True)
         subprocess.call("git add .", shell = True)
-        subprocess.call(''.join(['git commit --date="', str(390 + j), ' day ago" -m "updated"]']), shell = True)
+        subprocess.call(''.join(['git commit --date="', str(450 + i), ' day ago" -m "updated"]']), shell = True)
         subprocess.call("git push origin master", shell = True)
