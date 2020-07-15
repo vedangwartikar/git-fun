df2d444efa889bac307b77569153fabbd4058a18
Vedang Wartikar <vedangwartikar17@gmail.com>
2020-07-16 00:37:13 +0530

    updated]

--- gitFun.py
+++ gitFun.py
@@ -3,7 +3,7 @@ import random
 
 for i in range(10):
     for j in range(1, random.randint(3,6)):
-        print(j, 23+i)
+        print('contributions: ', j, 'days ago: ', 23+i)
         file = open("bait.py", "a")
         file.write('i')
         subprocess.call("git init", shell = True)
