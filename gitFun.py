85b83b3e7ebbabd15ec68880438ea8db926587a1
Vedang Wartikar <vedangwartikar17@gmail.com>
2020-07-25 00:32:18 +0530

    updated]

--- gitFun.py
+++ gitFun.py
@@ -1,12 +1,12 @@
 import subprocess
 import random
 
-for i in range(1, 6):
-    for j in range(3, random.randint(3,8)):
-        print(j, 470+i)
+for i in range(5):
+    for j in range(1, random.randint(3,8)):
+        print(j, 17+i)
         file = open("bait.py", "a")
         file.write('i')
         subprocess.call("git init", shell = True)
         subprocess.call("git add .", shell = True)
-        subprocess.call(''.join(['git commit --date="', str(470 + i), ' day ago" -m "updated"]']), shell = True)
+        subprocess.call(''.join(['git commit --date="', str(17 + i), ' day ago" -m "updated"]']), shell = True)
         subprocess.call("git push origin master", shell = True)
