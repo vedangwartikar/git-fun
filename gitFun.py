e01684e5e5c67429d2480679e4b6fbdfe6cf4cc0
Vedang Wartikar <vedangwartikar17@gmail.com>
2020-08-10 23:51:27 +0530

    updated

--- gitFun.py
+++ gitFun.py
@@ -1,1 +1,9 @@
-print("Hello")
+import subprocess
+
+file = open("bait.py", "a")
+file.write('i')
+#subprocess.call("C:/Users/Vedant Wartikar/Desktop/GitHub/git-fun", shell = True)
+subprocess.call("git init", shell = True)
+subprocess.call("git add .", shell = True)
+subprocess.call('git commit -m "updated"', shell = True)
+subprocess.call("git push origin master", shell = True)
